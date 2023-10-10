fe27951b93822bf87c2c7ef7f5498397df9d710a
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-10-10 17:43:45 +0630

    ud

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 module.exports = function (api) {
     api.cache(true);
     return {
         presets: ["babel-preset-expo"],
         plugins: ["expo-router/babel", "nativewind/babel"],
     };
+=======
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
+>>>>>>> parent of 7e1fec7 (upd)
 };
